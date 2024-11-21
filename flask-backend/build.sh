da22027491d4c234dfdb7e00442b61f1317d7a0e
subbu-h21 <subramanyah65@gmail.com>
2024-11-21 15:12:45 +0530

    Update build.sh

--- flask-backend/build.sh
+++ flask-backend/build.sh
@@ -3,14 +3,12 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install Rust in a custom directory
+# Install Rust
 echo "Installing Rust..."
-export CARGO_HOME="$HOME/.cargo"
-export RUSTUP_HOME="$HOME/.rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Add cargo binaries to PATH
-export PATH="$CARGO_HOME/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # Verify Rust installation
 rustc --version
